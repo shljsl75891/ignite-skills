a4d102e58bee2cb7407c2fd145c8dd9e9760cdba
shljsl75891 <shljsl75891@gmail.com>
2024-11-17 14:26:03 +0530

    feat(namaste-nodejs): json import as commonjs

--- namaste-nodejs/season_01/ep04-module-exports/commonjs/app.js
+++ namaste-nodejs/season_01/ep04-module-exports/commonjs/app.js
@@ -1,4 +1,7 @@
 const { calculateSum, calculateMultiply } = require("./calculator");
+const data = require("./data.json"); // require() can also be used for importing json
+
+console.log("Data: ", data);
 
 console.log("Main module executed");
 
