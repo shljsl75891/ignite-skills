c6b07cd19682570f9b07b797eee5704a913f45ec
shljsl75891 <shljsl75891@gmail.com>
2024-11-17 13:43:23 +0530

    feat(namaste-nodejs): ep04 - commonjs module code

---
+++ namaste-nodejs/season_01/ep04-module-exports/app.js
@@ -0,0 +1,8 @@
+const { calculateSum } = require("./modules/sum");
+
+console.log("Main module executed");
+
+var numA = 10;
+var numB = 10;
+
+calculateSum(numA, numB);
---
+++ namaste-nodejs/season_01/ep04-module-exports/modules/sum.js
@@ -0,0 +1,12 @@
+console.log("Sum Module Executed");
+
+/**
+ * This function prints the sum of two input numbers
+ * @param {number} a - Input A
+ * @param {number} b - Input B
+ */
+function calculateSum(a, b) {
+  console.log(a + b);
+}
+
+module.exports = { calculateSum };
