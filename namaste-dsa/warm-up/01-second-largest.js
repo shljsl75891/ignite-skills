515ec54d092a56c7c848f936289cdee754d179e5
shljsl75891 <shljsl75891@gmail.com>
2025-06-04 08:27:54 +0530

    feat(namaste-dsa): my implementation of second largest

---
+++ namaste-dsa/warm-up/01-second-largest.js
@@ -0,0 +1,20 @@
+function findSecondLargest(arr) {
+  let largest = arr[0],
+    secondLargest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    }
+    if (arr[i] > secondLargest && arr[i] < largest) {
+      secondLargest = arr[i];
+    }
+  }
+
+  return secondLargest;
+}
+
+console.log(findSecondLargest([28, 103, 45, 12, 67, 89, 23, 56, 78, 90]));
+console.log(findSecondLargest([5, -3, 0, 7, 10, 8, 17, 1]));
+console.log(findSecondLargest([2, 19, 17, 13, 92, 48, 56, 32]));
+console.log(findSecondLargest([4, 9, 0, 2, 8, 7, 1]));
