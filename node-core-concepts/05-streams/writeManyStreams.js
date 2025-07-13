36a1998285e50dd80c0db1296e39f7c4da985681
shljsl75891 <shljsl75891@gmail.com>
2025-07-13 13:09:37 +0530

    fix(node-core-concepts): memory issue in streams usage

--- node-core-concepts/05-streams/writeManyStreams.js
+++ node-core-concepts/05-streams/writeManyStreams.js
@@ -2,16 +2,46 @@ import fs from "node:fs/promises";
 
 /**
  * Benchmarking same
- * 18.794ms = 200 MB RAM = 100 % CPU (1 core)
- * We're using a bad way to use streams
+ * 154.786ms = 48 MB = 100% CPU (1 core)
+ *
+ * Now, it is very fast and memory efficient
  */
 
 const filePath = `${import.meta.dirname}/test-file.txt`;
 const fileHandle = await fs.open(filePath, "w");
 
+/**
+ * An abstract interface for writing streaming data to a destination in node.js
+ * An object with events, properties and methods with an internal buffer of size 16 KiB.
+ */
 const stream = fileHandle.createWriteStream();
-for (let i = 0; i < 1e6; i++) {
-  stream.write(`${i}\n`);
-}
 
-await fileHandle.close();
+// Size of internal buffer (bytes)
+const internalBufferSize = stream.writableHighWaterMark; // 16384 bytes
+
+// How much internal buffer is filled (bytes)
+const spaceLeftInInternalBuffer = stream.writableLength;
+
+let i = 0;
+const MAX_LIMIT = 1e6;
+const writeData = () => {
+  for (; i < MAX_LIMIT; i++) {
+    // This method return `false` if internal buffer is completely filled
+    if (!stream.write(i + " ")) {
+      // resume when internal buffer is drained
+      break;
+    }
+  }
+
+  if (i === MAX_LIMIT) {
+    stream.end(); // fire the `finish` event
+  }
+};
+
+writeData();
+
+stream.on("drain", () => {
+  writeData(); // resume the write operations
+});
+
+stream.on("finish", () => fileHandle.close());
