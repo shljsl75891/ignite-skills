9a2a424ead9f9ca870924faa3ba25b5f9b759974
shljsl75891 <shljsl75891@gmail.com>
2025-07-14 10:14:14 +0530

    feat(node-core-concepts): using pipes first time

---
+++ node-core-concepts/05-streams/04-pipes/readBigPipe.js
@@ -0,0 +1,22 @@
+import fs from "node:fs/promises";
+
+const sourcePath = `${import.meta.dirname}/src.txt`;
+const destinationPath = `${import.meta.dirname}/dest.txt`;
+
+const readHanlder = await fs.open(sourcePath, "r");
+const writeHandler = await fs.open(destinationPath, "w");
+
+const readableStream = readHanlder.createReadStream();
+const writableStream = writeHandler.createWriteStream();
+
+/**
+ * Pipe can only be applied on readable streams.
+ * Pipe returns same stream that is passed to it.
+ * Only writable streams can be passed to pipe method as argument. It means we can also pass duplex and transform streams.
+ * Pipes can be chained only if returned stream is readable. It means pipes can only be chained if the argument passed is duplex or transform stream..
+ */
+console.log(readableStream.pipe(writableStream) === writableStream); // true
+
+writableStream.on("finish", () => {
+  Promise.all([readHanlder.close(), writeHandler.close()]);
+});
