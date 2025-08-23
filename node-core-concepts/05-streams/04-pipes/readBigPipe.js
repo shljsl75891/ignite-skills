e3ac47c7e7e319cc0e0a0f5c35069072e466a53f
shljsl75891 <shljsl75891@gmail.com>
2025-08-23 19:39:26 +0530

    feat(node-core-concepts): `pipeline` method in streams

--- node-core-concepts/05-streams/04-pipes/readBigPipe.js
+++ node-core-concepts/05-streams/04-pipes/readBigPipe.js
@@ -1,3 +1,5 @@
+import { Buffer } from "node:buffer";
+import { pipeline } from "node:stream/promises";
 import fs from "node:fs/promises";
 
 const sourcePath = `${import.meta.dirname}/src.txt`;
@@ -15,7 +17,16 @@ const writableStream = writeHandler.createWriteStream();
  * Only writable streams can be passed to pipe method as argument. It means we can also pass duplex and transform streams.
  * Pipes can be chained only if returned stream is readable. It means pipes can only be chained if the argument passed is duplex or transform stream..
  */
-console.log(readableStream.pipe(writableStream) === writableStream); // true
+// console.log(readableStream.pipe(writableStream) === writableStream); // true
+
+console.time("copy");
+pipeline(readableStream, writableStream)
+  .then(() => {
+    console.timeEnd("copy");
+  })
+  .catch((err) => {
+    console.error("Pipeline failed", err);
+  });
 
 writableStream.on("finish", () => {
   Promise.all([readHanlder.close(), writeHandler.close()]);
