c6d4979e6ea21aa5344858c885aa9e34f836ce19
shljsl75891 <shljsl75891@gmail.com>
2025-09-14 15:53:16 +0530

    feat(namaste-dsa): better solution :( for max-consecutive-ones
    always think edge cases and dry run on them before submitting solution

--- namaste-dsa/01-arrays/07-max-consecutive-ones.js
+++ namaste-dsa/01-arrays/07-max-consecutive-ones.js
@@ -3,26 +3,16 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function (nums) {
-  let result = 0;
-  let i = 0,
-    j = 0;
-  while (i < nums.length) {
-    if (nums[i] === 0) {
-      result = Math.max(result, i - j - 1);
-      // if first element was 1, as otherwise nums[j] will always be zero
-      if (nums[j] === 1) ++result;
-      j = i;
+  let count = 0,
+    maxCount = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) count++;
+    else {
+      maxCount = Math.max(count, maxCount);
+      count = 0;
     }
-    i++;
   }
-
-  // if all elements of array are 1's
-  if (j === 0 && nums[j] === 1) {
-    return nums.length;
-  }
-
-  // if last element will be 1
-  return Math.max(result, i - j - 1);
+  return Math.max(count, maxCount);
 };
 
 console.log(findMaxConsecutiveOnes([0]));
--- namaste-dsa/01-arrays/README.md
+++ namaste-dsa/01-arrays/README.md
@@ -6,3 +6,4 @@ Learnings
   - Non Decreasing Order = `arr[i + 1] <= arr[i]`
   - Non Increasing Order = `arr[i + 1] >= arr[i]`
 - In Place - without using extra space, means change the same array
+- Only submit your solution after you dry run the code on found edge cases
