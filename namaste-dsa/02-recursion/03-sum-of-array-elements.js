e436b6dfdec898099cd2f1975f1e7b8102d063cd
shljsl75891 <shljsl75891@gmail.com>
2025-11-02 14:55:47 +0530

    feat(namaste-dsa): sum of odd array elements using recursion

--- namaste-dsa/02-recursion/03-sum-of-array-elements.js
+++ namaste-dsa/02-recursion/03-sum-of-array-elements.js
@@ -3,4 +3,11 @@ function sumOfAllElements(arr) {
   return arr.pop() + sumOfAllElements(arr);
 }
 
+function sumOfOddElements(arr) {
+  if (!arr || arr.length === 0) return 0;
+  const element = arr.pop();
+  return element % 2 ? element + sumOfOddElements(arr) : sumOfOddElements(arr);
+}
+
 console.log(sumOfAllElements([5, 2, 3, 8, 6, 9, 10, 7, 4, 1]));
+console.log(sumOfOddElements([5, 3, 0, 2, 1]));
