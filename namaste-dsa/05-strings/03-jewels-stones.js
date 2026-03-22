b952d749d50a3665eb5b2db1913a9690d4e7c315
shljsl75891 <shljsl75891@gmail.com>
2026-03-22 22:44:52 +0530

    feat(namaste-dsa): solution for jewels stones

---
+++ namaste-dsa/05-strings/03-jewels-stones.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function (jewels, stones) {
+  let count = 0;
+  for (let char of stones) {
+    if (jewels.includes(char)) ++count;
+  }
+  return count;
+};
