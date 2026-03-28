396229bfa7970d0978769c29f96f063c475d9e78
shljsl75891 <shljsl75891@gmail.com>
2026-03-28 13:12:40 +0530

    feat(namaste-dsa): add min stack optimized implementation

--- namaste-dsa/06-stacks-queues/05-min-stack.js
+++ namaste-dsa/06-stacks-queues/05-min-stack.js
@@ -50,3 +50,52 @@ MinStack.prototype.getMin = function () {
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
  */
+
+/** Optimized implementation */
+
+Array.prototype.top = function () {
+  return this[this.length - 1];
+};
+
+var MinStack = function () {
+  this.stack = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  const currentMinimum = this.stack.top()?.[1] ?? val;
+  this.stack.push([val, Math.min(currentMinimum, val)]);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  this.stack.pop()?.[0];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack.top()?.[0];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.stack.top()?.[1];
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
