87f1a3c5460f8f441c5ce3a03766b8bd31b6997b
shljsl75891 <shljsl75891@gmail.com>
2026-07-12 23:36:27 +0530

    feat(namaste-dsa): O(1) memory solution for trapping water problem

--- namaste-dsa/08-two-ptrs-&-sliding-window/06-trapping-water.js
+++ namaste-dsa/08-two-ptrs-&-sliding-window/06-trapping-water.js
@@ -1,4 +1,5 @@
 /**
+ * O(N) = Memory solution
  * @param {number[]} height
  * @return {number}
  */
@@ -17,5 +18,32 @@ var trap = function (height) {
     trappedWater += Math.min(leftMax[i], rightMax[i]) - height[i];
   }
 
+  return trappedWater;
+};
+
+/**
+ * O(1) = Memory solution
+ * @param {number[]} height
+ * @return {number}
+ *
+ * The water trapped at any point will be according to the minimum height of left and right container.
+ */
+var trap = function (height) {
+  let trappedWater = 0;
+  let left = 0,
+    right = height.length - 1;
+  let leftMax = height[left],
+    rightMax = height[right];
+  while (left < right) {
+    // water will be filled according to left container
+    if (height[left] < height[right]) {
+      leftMax = Math.max(height[left], leftMax);
+      trappedWater += leftMax - height[left++];
+      // water will be filled according to right container
+    } else {
+      rightMax = Math.max(height[right], rightMax);
+      trappedWater += rightMax - height[right--];
+    }
+  }
   return trappedWater;
 };
